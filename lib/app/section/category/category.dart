2aa617a18e2ab00870e1fd888c74adb6118335c1
unknown <avikumarr355@gmail.com>
2024-01-20 16:59:14 +0530

    create category and category web file

---
+++ lib/app/section/category/category.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_website/app/section/category/category_web.dart';
+import 'package:furniture_website/core/res/responsive.dart';
+
+class Category extends StatelessWidget {
+  const Category({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(web: CategoryWeb());
+  }
+}
---
+++ lib/app/section/category/category_web.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryWeb extends StatefulWidget {
+  const CategoryWeb({super.key});
+
+  @override
+  State<CategoryWeb> createState() => _CategoryWebState();
+}
+
+class _CategoryWebState extends State<CategoryWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
