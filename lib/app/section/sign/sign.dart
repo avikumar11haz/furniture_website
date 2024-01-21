7410e4ec81a208b752d61a7aebdd440ef7c13207
unknown <avikumarr355@gmail.com>
2024-01-21 23:45:21 +0530

    create sign web file

--- lib/app/section/sign/sign.dart
+++ lib/app/section/sign/sign.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_website/app/section/sign/sign_web.dart';
 import 'package:furniture_website/core/res/responsive.dart';
 
 class Sign extends StatelessWidget {
---
+++ lib/app/section/sign/sign_web.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignWeb extends StatefulWidget {
+  const SignWeb({super.key});
+
+  @override
+  State<SignWeb> createState() => _SignWebState();
+}
+
+class _SignWebState extends State<SignWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
