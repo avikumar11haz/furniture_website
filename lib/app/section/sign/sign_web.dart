0f8e079db79ce89915e5f2690a101be202d197b6
unknown <avikumarr355@gmail.com>
2024-01-21 23:57:25 +0530

    text name

--- lib/app/section/sign/sign_web.dart
+++ lib/app/section/sign/sign_web.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_website/app/changes/strings.dart';
+import 'package:furniture_website/core/color/colors.dart';
+import 'package:sizer/sizer.dart';
 
 class SignWeb extends StatefulWidget {
   const SignWeb({super.key});
@@ -10,6 +13,64 @@ class SignWeb extends StatefulWidget {
 class _SignWebState extends State<SignWeb> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: EdgeInsets.only(right: screenWidth > 2000 ? 20.w : 5.w),
+      child: Container(
+        padding: EdgeInsets.only(left: 10.w, top: 5.h, bottom: 5.h, right: 5.w),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+          color: secondaryColor1
+        ),
+        child: Row(
+          children: [
+            Expanded(child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(signDescription, style: const TextStyle(
+                  fontSize: 25, color: textColor2,
+                ),),
+                SizedBox(height: 1.h,),
+                Text(signDescription2, style: const TextStyle(
+                  fontSize: 70, color: textColor2, fontWeight: FontWeight.w900
+                ),),
+                SizedBox(height: 1.h,),
+                Text(signDescription3, style: const TextStyle(
+                  fontSize: 25, color: textColor2,
+                ),),
+                SizedBox(height: 2.h,),
+                Container(
+                  height: 50,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF1DEBD),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Name',
+                      hintStyle: TextStyle(
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16
+                      ),
+                      disabledBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 1.h,),
+
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
   }
 }
