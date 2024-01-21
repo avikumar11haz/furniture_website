ab9d910e133fbbbb760e334aeeb8b1ac214bdffb
unknown <avikumarr355@gmail.com>
2024-01-21 23:43:35 +0530

    create sign file

---
+++ lib/app/section/sign/sign.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_website/core/res/responsive.dart';
+
+class Sign extends StatelessWidget {
+  const Sign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(web: const SignWeb());
+  }
+}
