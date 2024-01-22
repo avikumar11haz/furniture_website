4e30f7c2ee5e546bf5330060e7e8935f32186c16
unknown <avikumarr355@gmail.com>
2024-01-23 00:24:48 +0530

    create footer web file

---
+++ lib/app/section/footer/footer_web.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FooterWeb extends StatefulWidget {
+  const FooterWeb({super.key});
+
+  @override
+  State<FooterWeb> createState() => _FooterWebState();
+}
+
+class _FooterWebState extends State<FooterWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
